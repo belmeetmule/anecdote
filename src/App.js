38fc2983e50c55c025354f895c39819a84cd0ced
mule <belmeetmule@gmail.com>
2023-06-19 00:13:37 +0300

    Add style to anecdote cards

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const App = () => {
 
   return (
     <div>
+     <h2>Anecdotes</h2>
       <AnecdoteList />
       <AnecdoteForm />
     </div>
--- src/components/AnecdoteList.js
+++ src/components/AnecdoteList.js
@@ -1,5 +1,7 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { upVote } from '../reducers/anecdoteReducer'
+import './AnecdoteList.css'
+
 
 const AnecdoteList = ()=>{
 
@@ -14,19 +16,34 @@ const AnecdoteList = ()=>{
 
   // This is for sorting strings
   // const sortedAnecdotes = anecdotes.sort((a,b)=> (a.content > b.content)? -1 : (b.content > a.content) ? 1: 0)  
+// const cardInline=  {
+//         display: "flex",
+//         border: "1px solid green",
+//         flexDirection: "column",
+//         marginBottom: "10px",
+//         background: "#ccc",
+//         borderRadius: "8px",
+//         padding: "10px 0 0 0"
+//     }
+
+    const anecdoteBody ={
+        color: "#28292d63",
+        padding: "10px 10px",
+    }
+
+
 
     return(
         <div>
-             <h2>Anecdotes</h2>
                 {anecdotes.sort((a,b)=> (b.votes - a.votes)).map(anecdote =>
-                    <div key={anecdote.id}>
-                    <div>
-                        {anecdote.content}
-                    </div>
-                    <div>
-                        has {anecdote.votes}
-                        <button onClick={() => vote(anecdote.id)}>vote</button>
-                    </div>
+                    <div key={anecdote.id} className='card'>
+                        <div style={anecdoteBody}>
+                            {anecdote.content}
+                        </div>
+                        <div style={{background: "#ff000047", color:"white", padding: "0 10px", display: "flex", gap: "10px", alignItems:"center"}}>
+                            has {anecdote.votes}
+                            <button onClick={() => vote(anecdote.id)} style={{padding: "3px 5px"}}>vote</button>
+                        </div>
                     </div>
                 )}
         </div>
