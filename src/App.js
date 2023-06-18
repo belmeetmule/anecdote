6263c01948b69864f70218a082e653535538d121
mule <belmeetmule@gmail.com>
2023-06-18 17:11:41 +0300

    Modified to add renamed components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import Anecdotes from './components/Anecdotes'
-import NewAnecdote from './components/NewAnecdote'
+import AnecdoteList from './components/AnecdoteList'
+import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
 
   return (
     <div>
-      <Anecdotes />
-      <NewAnecdote />
+      <AnecdoteList />
+      <AnecdoteForm />
     </div>
   )
 }
