769269531aa90e5dc3cef18f5d95064b3f642e10
mule <belmeetmule@gmail.com>
2023-06-19 16:42:22 +0300

    Add filter component to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
+import Filter from './components/Filter'
 
 const App = () => {
 
   return (
     <div>
      <h2>Anecdotes</h2>
+      <Filter />
       <AnecdoteList />
       <AnecdoteForm />
     </div>
