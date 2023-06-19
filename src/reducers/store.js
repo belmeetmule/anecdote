7f64b180320d1c104deee34480fe4d3ee266e139
mule <belmeetmule@gmail.com>
2023-06-19 16:40:23 +0300

    User combile reducer

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -1,6 +1,13 @@
-import {createStore} from 'redux'
+import {createStore, combineReducers} from 'redux'
 import anecdoteReducer from './anecdoteReducer'
+import FilterReducer from './FilterReducer';
 
-const store = createStore(anecdoteReducer)
+const reducer = combineReducers(
+    {
+        anecdotes: anecdoteReducer,
+        filter: FilterReducer
+    }
+)
+const store = createStore(reducer)
 
 export default store;
