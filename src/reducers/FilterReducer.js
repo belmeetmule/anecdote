96f6c46459ecdace10243d956c8a829daaa50d6d
mule <belmeetmule@gmail.com>
2023-07-05 02:17:53 +0300

    refactor to use react toolkit

--- src/reducers/FilterReducer.js
+++ src/reducers/FilterReducer.js
@@ -1,20 +1,33 @@
-const FilterReducer = (state="", action) =>{
-    switch(action.type){
-        case 'FILTER':
-            const newState = {...state, filter: action.payload}
-            console.log('in the reducer', newState)
+import { createSlice } from "@reduxjs/toolkit"
+
+const filterSlice = createSlice({
+    name:'filter',
+    initialState:'',
+    reducers: {
+         filterChanged(state,action){
+           // const newState = {...state, filter: action.payload}
             return action.payload
-        default:
-            return state
+        }
     }
-}
+})
+// const FilterReducer = (state="", action) =>{
+//     switch(action.type){
+//         case 'FILTER':
+//             const newState = {...state, filter: action.payload}
+//             console.log('in the reducer', newState)
+//             return action.payload
+//         default:
+//             return state
+//     }
+// }
 
-export const filterChanged = (filter)=>{
-    return {
-        type: 'FILTER',
-        payload: filter
-    }
+// export const filterChanged = (filter)=>{
+//     return {
+//         type: 'FILTER',
+//         payload: filter
+//     }
 
-}
+// }
 
-export default FilterReducer
+export const {filterChanged} = filterSlice.actions
+export default filterSlice.reducer
