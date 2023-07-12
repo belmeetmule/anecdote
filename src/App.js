f115d40557681b01672d9748ba3bca26dc5d0630
mule <belmeetmule@gmail.com>
2023-07-13 01:21:42 +0300

    style the app component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import Notification from './components/Notification'
 import AnecdoteList from './components/AnecdoteList'
-import AnecdoteForm from './components/AnecdoteForm'
 import Filter from './components/Filter'
+import AnecdoteForm from './components/AnecdoteForm'
+import { useDispatch } from 'react-redux'
+import { clearNotificaiton } from './reducers/notificationReducer'
+import './App.css'
 
 const App = () => {
 
+  const dispatch = useDispatch()
+  setTimeout(()=>{
+    dispatch(clearNotificaiton())
+  }, 2000)
+
   return (
-    <div>
+    <div className="container">
      <h2>Anecdotes</h2>
+      <Notification />
       <Filter />
       <AnecdoteList />
       <AnecdoteForm />
