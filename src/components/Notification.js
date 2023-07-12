01b0cea630d5a63d7d56a4bd07125cd01cf094ec
mule <belmeetmule@gmail.com>
2023-07-13 01:22:45 +0300

    fix: hide notification after 3seconds

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,14 +1,41 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { clearNotificaiton } from '../reducers/notificationReducer'
+
+
+
 const Notification = () => {
   const style = {
     border: 'solid',
     padding: 10,
-    borderWidth: 1
+    borderWidth: 1,
+    marginBottom: 20,
+    position: 'fixed',
+    top: 0,
+    right:0,
+    borderRadius: 8,
+    color: '#673AB7',
+    transition:'all .3s',
+    background: 'white'
   }
+
+  const notification = useSelector(state => {
+    return state.notification
+  })
+
+  const dispatch = useDispatch();
+
+  if(notification !== ""){
+    setTimeout(()=> {
+     dispatch(clearNotificaiton())
+    }, 3000)
+
+
   return (
     <div style={style}>
-      render here notification...
+      {notification}
     </div>
   )
+}
 }
 
 export default Notification
