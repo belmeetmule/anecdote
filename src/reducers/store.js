91027d231af2c14dbac26e6ba59f80d029fde34e
mule <belmeetmule@gmail.com>
2023-07-13 01:21:01 +0300

    fix typo in the name of state variable notification

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -1,12 +1,13 @@
 import anecdoteReducer from './anecdoteReducer'
 import FilterReducer from './FilterReducer';
+import notificationReducer from './notificationReducer';
 import { configureStore } from '@reduxjs/toolkit';
 
 const store = configureStore({
     reducer: {
         anecdotes: anecdoteReducer,
         filter: FilterReducer,
-        notificaton: notificatonReducer
+        notification: notificationReducer
     }
 }
 )
