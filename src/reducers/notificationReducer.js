b283e1c7f0bb7646e74482de4cecc48fea756436
mule <belmeetmule@gmail.com>
2023-07-12 11:24:31 +0300

    add notificaiton reducer

---
+++ src/reducers/notificationReducer.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const notificationSlice = createSlice({
+    name: 'notification',
+    initialState: '',
+    reducers:{
+        showNotification(state, action){
+            return state = action.payload    
+        },
+
+        clearNotificaiton(state){
+            return state=''
+        }
+    }
+
+})
+
+export const {showNotification, clearNotificaiton} = notificationSlice.actions;
+export default notificationSlice.reducer;
