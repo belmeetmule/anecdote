e83c43afa7000c5713e4e61ec53f571dd161df82
mule <belmeetmule@gmail.com>
2023-07-24 03:08:41 +0300

    update style of anecdote form

--- src/components/AnecdoteForm.js
+++ src/components/AnecdoteForm.js
@@ -15,14 +15,33 @@ const AnecdoteForm = ()=>{
         }
     }
 
+    const style = {
+        marginBottom: "10px",
+        background: "#ccc",
+        borderRadius: "8px",
+        padding: "10px",
+        background: "#131f40",
+    }
+
+    const inputStyle = {
+        background: "#ccc",
+        borderRadius: "8px",
+        border: "1px solid #131f20",
+        padding: "5px 10px",
+        marginLeft: "10px",
+        fontSize: "1.2rem",
+        display: "flex",
+    }
+
     return (
-        <div>
+        
+        <div style={style}>
                 <h2>create new</h2>
                 <form onSubmit={addAnecdotes}>
                     <div>
-                        <input name="anecdote" />
+                        <input name="anecdote" style={inputStyle} placeholder='Enter new anecdote here ...'/>
                     </div>
-                    <button type="submit">create</button>
+                    <button type="submit" style={inputStyle}>create</button>
                 </form>
         </div>
     )
